26b8dd85d2a71ce01ffe4b5ea69c4a29427c788e
felix925 <45941545+felix925@users.noreply.github.com>
2020-04-30 01:48:00 +0900

    first commit

---
+++ app/src/main/java/jp/making/felix/connpasssearcher/MainActivity.kt
@@ -0,0 +1,18 @@
+package jp.making.felix.connpasssearcher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jp.making.felix.connpasssearcher.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
